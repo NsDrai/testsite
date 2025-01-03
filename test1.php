051cdef8c500000b59ffdee8842c85f8991ffdd3
NsDrai <gladun.vladimir03@mail.ru>
2025-01-03 03:01:38 +0300

    Update test1.php

--- test1.php
+++ test1.php
@@ -30,3 +30,24 @@ function getCustomDate($timeStamp) {
 echo getCustomDate(1532435204) . "\n"; // 24/07/2018
 echo getCustomDate(532435204) . "\n";  // 15/11/1986
 echo getCustomDate(5324352) . "\n";    // 03/03/1970
+
+
+function isPalindrome($text) {
+    for ($i = 0; $i <= ceil(mb_strlen($text)/2); $i += 1) {
+        if (strlen($text) == 1) {
+            return true;
+        } elseif (mb_substr($text, $i, 1) == mb_substr($text, -($i + 1), 1)) {
+            continue;
+        } else {
+            return false;
+        }
+    }
+    return true;
+}
+
+echo isPalindrome('radar') . "\n"; // true
+echo isPalindrome('maam') . "\n"; // true
+echo isPalindrome('a') . "\n";     // true
+echo isPalindrome('abs') . "\n";   // false
+echo isPalindrome('шалаш') . "\n"; // true
+echo ceil(15/2);
